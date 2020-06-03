398f925fb3fce469499a348a7bc3de7ac047a7bf
moxxiq <dimanavsisto@gmail.com>
2020-06-03 04:01:35 +0300

    changed namespace

---
+++ GigavenuePlanB/Zone.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigavenuePlanB
+{
+    public class Zone
+    {
+        public Zone(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
+        public int ZoneId { get; set; }
+        public int ZoneEventId { get; set; }
+        public int Color { get; set; }
+        public string Name{ get; set; }
+        public int Capacity{ get; set; }
+        
+    }
+}
