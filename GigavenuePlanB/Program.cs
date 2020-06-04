c3c52b044094930a3bb3eb0d8265c7445cbe4343
moxxiq <dimanavsisto@gmail.com>
2020-06-04 18:18:26 +0300

    Removed file reading

--- GigavenuePlanB/Program.cs
+++ GigavenuePlanB/Program.cs
@@ -10,16 +10,12 @@ namespace GigavenuePlanB
         {
             Storage dataStorage = new Storage();
 
-            FileStream fileStream = new FileStream("users.txt", FileMode.Open);
-            using (StreamReader reader = new StreamReader(fileStream))
-            {
-                // Valid File format id,email,firstname,lastname;
-                string[] line = reader.ReadLine().Split(',');
-                dataStorage.AddUser(new GigUser(line[1], line[2],line[3], int.Parse(line[0])));
-            }
-
             GigUser Me = dataStorage.AddUser("me@me.com","Dima","Bub");
             Console.WriteLine($"User {Me.Firstname} created\n");
+            dataStorage.WriteToConsole();
+            
+            Console.WriteLine("Зробимо деякі зміни");
+            Me.Email = "another.me@me.com";
             dataStorage.WriteToConsole();
         }
     }
