8c67dca1341d2e404c8fec0a12914c6cadab2207
moxxiq <dimanavsisto@gmail.com>
2020-06-04 19:23:42 +0300

    Added try-catch for 2 Exceptions

--- GigavenuePlanB/Program.cs
+++ GigavenuePlanB/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace GigavenuePlanB
 {
@@ -13,9 +14,30 @@ namespace GigavenuePlanB
             GigUser Me = dataStorage.AddUser("me@me.com","Dima","Bub");
             Console.WriteLine($"User {Me.Firstname} created\n");
             dataStorage.WriteToConsole();
+            Console.WriteLine();
             
-            Console.WriteLine("Зробимо деякі зміни");
-            Me.Email = "another.me@me.com";
+            Console.WriteLine("Будь ласка введіть дані для нового користувача в одному рядку");
+            Console.WriteLine("email firstname lastname");
+            // Valid File format email firstname lastname;
+            string[] line = Console.ReadLine().Split(' ');
+            try
+            {
+                GigUser newUser = new GigUser(line[0], line[1],line[2], 12);
+                try
+                {
+                    dataStorage.AddUser(newUser);
+                }
+                catch (UserAlreadyExistsException exists)
+                {
+                    Console.WriteLine(exists);
+                }
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("Less than 3 arguments entered");
+            }
+            
+            Console.WriteLine("\nAll users:");
             dataStorage.WriteToConsole();
         }
     }
